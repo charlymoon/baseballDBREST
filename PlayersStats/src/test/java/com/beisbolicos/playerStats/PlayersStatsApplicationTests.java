317be0efab3d041be41c334269a8d3662b30bd20
charlymoon <cdelunasaenz@yahoo.com.mx>
2021-09-19 20:55:43 -0500

    Primer push desde eclipse

---
+++ PlayersStats/src/test/java/com/beisbolicos/playerStats/PlayersStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beisbolicos.playerStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
